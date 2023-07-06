4d60b4e1351da3dd56876f3bd816a7743edf0b26
CollinBurleigh <collin.s.burleigh@gmail.com>
2023-07-05 23:53:36 -0400

    navbar progress

--- client/portfolio-site/src/AppNavbar.js
+++ client/portfolio-site/src/AppNavbar.js
@@ -3,14 +3,15 @@ import { Navbar, Nav } from 'react-bootstrap';
 
 const AppNavbar = () => {
   return (
-    <Navbar bg="light" expand="lg">
-      <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+    <Navbar bg="light" expand="lg" variant="light" fixed="top">
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="ml-auto">
           <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#about">About</Nav.Link>
-          <Nav.Link href="#services">Services</Nav.Link>
+          <Nav.Link href="#linkedin">LinkedIn</Nav.Link>
+          <Nav.Link href="#blog">Blog</Nav.Link>
+          <Nav.Link href="#resume">Resume</Nav.Link>
+          <Nav.Link href="#projects">Projects</Nav.Link>
           <Nav.Link href="#contact">Contact</Nav.Link>
         </Nav>
       </Navbar.Collapse>
@@ -19,3 +20,4 @@ const AppNavbar = () => {
 };
 
 export default AppNavbar;
+
