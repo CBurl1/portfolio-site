b7622d5e5b6ec62735c3b47483f55874da0ded50
CollinBurleigh <collin.s.burleigh@gmail.com>
2023-07-06 00:11:09 -0400

    scrolling into section navbar

--- client/portfolio-site/src/App.js
+++ client/portfolio-site/src/App.js
@@ -3,32 +3,34 @@ import AppNavbar from './AppNavbar.js';
 import React from 'react';
 
 function App() {
+  const scrollToSection = (sectionId) => {
+    const section = document.getElementById(sectionId);
+    if (section) {
+      section.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
+
   return (
     <div className="App">
-      <AppNavbar />
+      <AppNavbar scrollToSection={scrollToSection} />
       <header className="App-header">
         <section id="home">
           <h1>Collin Burleigh</h1>
           <h3>Full Stack Software Engineer</h3>
         </section>
 
-        <section id="linkedin">
-          {/* Content for the LinkedIn section */}
-        </section>
-
-        <section id="blog">
-          {/* Content for the Blog section */}
-        </section>
-
         <section id="resume">
+          <h2 className="section-title">Resume</h2>
           {/* Content for the Resume section */}
         </section>
 
         <section id="projects">
+          <h2 className="section-title">Projects</h2>
           {/* Content for the Projects section */}
         </section>
 
         <section id="contact">
+          <h2 className="section-title">Contact</h2>
           {/* Content for the Contact section */}
         </section>
       </header>
@@ -37,4 +39,6 @@ function App() {
 }
 
 export default App;
+
+
 
--- client/portfolio-site/src/AppNavbar.js
+++ client/portfolio-site/src/AppNavbar.js
@@ -1,18 +1,16 @@
 import React from 'react';
 import { Navbar, Nav } from 'react-bootstrap';
 
-const AppNavbar = () => {
+const AppNavbar = ({ scrollToSection }) => {
   return (
     <Navbar bg="light" expand="lg" variant="light" fixed="top">
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="ml-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#linkedin">LinkedIn</Nav.Link>
-          <Nav.Link href="#blog">Blog</Nav.Link>
-          <Nav.Link href="#resume">Resume</Nav.Link>
-          <Nav.Link href="#projects">Projects</Nav.Link>
-          <Nav.Link href="#contact">Contact</Nav.Link>
+          <Nav.Link onClick={() => scrollToSection('home')}>Home</Nav.Link>
+          <Nav.Link onClick={() => scrollToSection('resume')}>Resume</Nav.Link>
+          <Nav.Link onClick={() => scrollToSection('projects')}>Projects</Nav.Link>
+          <Nav.Link onClick={() => scrollToSection('contact')}>Contact</Nav.Link>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
@@ -20,4 +18,5 @@ const AppNavbar = () => {
 };
 
 export default AppNavbar;
+
 
