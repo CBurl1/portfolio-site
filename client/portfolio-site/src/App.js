fd02a1c287128539fb8d2b6441f3608ade289f0e
CollinBurleigh <collin.s.burleigh@gmail.com>
2023-07-05 23:56:16 -0400

    home page

--- client/portfolio-site/src/App.js
+++ client/portfolio-site/src/App.js
@@ -1,24 +1,40 @@
 import './App.css';
 import AppNavbar from './AppNavbar.js';
-import React from 'react'
-
+import React from 'react';
 
 function App() {
   return (
     <div className="App">
       <AppNavbar />
       <header className="App-header">
-        <p>
-          Collin Burleigh
-        </p>
-        <br>
-        </br>
-        <p>
-          Full Stack Software Engineer
-        </p>
+        <section id="home">
+          <h1>Collin Burleigh</h1>
+          <h3>Full Stack Software Engineer</h3>
+        </section>
+
+        <section id="linkedin">
+          {/* Content for the LinkedIn section */}
+        </section>
+
+        <section id="blog">
+          {/* Content for the Blog section */}
+        </section>
+
+        <section id="resume">
+          {/* Content for the Resume section */}
+        </section>
+
+        <section id="projects">
+          {/* Content for the Projects section */}
+        </section>
+
+        <section id="contact">
+          {/* Content for the Contact section */}
+        </section>
       </header>
     </div>
   );
 }
 
 export default App;
+
