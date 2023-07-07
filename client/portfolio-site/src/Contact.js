0bdf7135b37ed902441b770a37097c9cd9272cb0
CollinBurleigh <collin.s.burleigh@gmail.com>
2023-07-07 03:28:37 -0400

    intro section progress

--- client/portfolio-site/src/Contact.js
+++ client/portfolio-site/src/Contact.js
@@ -8,24 +8,24 @@ const Contact = () => {
         <div style={{ width: '400px', margin: '20px', padding: '20px', border: '1px solid #ccc', borderRadius: '5px' }}>
           <div>
             <h3>Email</h3>
-            <p>example@example.com</p>
+            <p>collin.s.burleigh@gmail.com</p>
           </div>
           <div>
             <h3>Location</h3>
-            <p>City, Country</p>
+            <p>Greater Boston, United States</p>
           </div>
         </div>
         <div style={{ width: '400px', margin: '20px', padding: '20px', border: '1px solid #ccc', borderRadius: '5px' }}>
           <div>
             <h3>LinkedIn</h3>
             <p>
-              <a href="https://www.linkedin.com/">LinkedIn Profile</a>
+              <a href="https://www.linkedin.com/in/burleighcollin/">Connect with me!</a>
             </p>
           </div>
           <div>
             <h3>Medium</h3>
             <p>
-              <a href="https://medium.com/">Medium Profile</a>
+              <a href="https://medium.com/@burleighcollin">Check out my blog!</a>
             </p>
           </div>
         </div>
