50b5d517422ae3f1ae483d3222775993bcb48fa1
CollinBurleigh <collin.s.burleigh@gmail.com>
2023-07-17 21:54:50 -0400

    styling on contacts and projects

--- client/portfolio-site/src/Contact.js
+++ client/portfolio-site/src/Contact.js
@@ -3,28 +3,30 @@ import './Contact.css';
 
 const Contact = () => {
   return (
-    <div className="contact-card">
-      <h2 className="contact-heading">Contact</h2>
-      <div className="contact-container">
-        <div className="contact-item">
-          <h3>Email</h3>
-          <p>collin.s.burleigh@gmail.com</p>
-        </div>
-        <div className="contact-item">
-          <h3>Location</h3>
-          <p>Greater Boston, United States</p>
-        </div>
-        <div className="contact-item">
-          <h3>LinkedIn</h3>
-          <p>
-            <a href="https://www.linkedin.com/in/burleighcollin/">Connect with me!</a>
-          </p>
-        </div>
-        <div className="contact-item">
-          <h3>Medium</h3>
-          <p>
-            <a href="https://medium.com/@burleighcollin">Check out my blog!</a>
-          </p>
+    <div className="contact-container">
+      <div className="contact-card">
+        <h2 className="contact-heading">Contact</h2>
+        <div className="contact-info">
+          <div className="contact-item">
+            <h3>Email</h3>
+            <p>collin.s.burleigh@gmail.com</p>
+          </div>
+          <div className="contact-item">
+            <h3>Location</h3>
+            <p>Greater Boston, United States</p>
+          </div>
+          <div className="contact-item">
+            <h3>LinkedIn</h3>
+            <p>
+              <a href="https://www.linkedin.com/in/burleighcollin/">Connect with me!</a>
+            </p>
+          </div>
+          <div className="contact-item">
+            <h3>Medium</h3>
+            <p>
+              <a href="https://medium.com/@burleighcollin">Check out my blog!</a>
+            </p>
+          </div>
         </div>
       </div>
     </div>
@@ -32,6 +34,7 @@ const Contact = () => {
 }
 
 export default Contact;
+
 
 
 
