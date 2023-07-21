bfced5ec3c9ddfaf910631d0450fea20e854ef75
CollinBurleigh <collin.s.burleigh@gmail.com>
2023-07-21 16:13:51 -0400

    carosel progress

--- client/portfolio-site/src/Intro.js
+++ client/portfolio-site/src/Intro.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import './Intro.css';
 import blockbuster from './blockbuster.png';
 
@@ -8,39 +9,39 @@ const Intro = () => {
     {
       title: 'Parki',
       image: 'https://i.redd.it/l3ouhs6ncj661.jpg',
-      description: 'Full stack web application that allows skiers and snowboarders to stay up to date with terrain parks around them',
     },
     {
       title: 'Blockbuster Employee Portal',
       image: blockbuster,
-      description: 'Full stack web application that allows employees to view, modify or create new movies, rentals, and clients',
     },
     {
       title: 'Music Player and Storage',
       image: 'https://static.vecteezy.com/system/resources/previews/021/660/070/original/mp3-player-collection-set-with-different-color-in-pixel-art-style-vector.jpg',
-      description: 'CLI application with SQLAlchemy that stores music and play songs in a user’s browser',
     },
     {
       title: 'Seasonal Produce Educator',
       image: 'https://media.istockphoto.com/id/165723582/vector/pixel-fruits.jpg?s=612x612&w=0&k=20&c=cr7k9zjJ1jQ_6H-dbiVK4m5OmWUkWmSyLdK4Z0UtnWs=',
-      description: 'Vanilla Javascript webpage to educate people on when produce is best to consume',
     },
     {
       title: 'Ski Shop',
       image: 'https://cdna.artstation.com/p/assets/images/images/048/109/936/large/kivanc-yucel-snowy-area2.jpg?1649234808',
-      description: 'ReactJS Storefront for a ski shop',
     },
     {
       title: 'Portfolio Site',
       image: 'https://img.freepik.com/premium-vector/old-computer-pixel-art-style_475147-1000.jpg',
-      description: 'This awesome website',
     },
   ];
+
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate('/projects');
+  };
 
   useEffect(() => {
     const interval = setInterval(() => {
       setCurrentProject((prevProject) => (prevProject + 1) % projects.length);
-    }, 5000); // Change project every 5 seconds
+    }, 1000); // Change project every 5 seconds
 
     return () => clearInterval(interval);
   }, [projects.length]);
@@ -52,7 +53,7 @@ const Intro = () => {
         <p>
           Welcome to my portfolio website! As a full stack developer, I am excited to showcase my experience and skills in web development. Enter through the button below to learn more about me and my work!
         </p>
-        <button className="custom-button" onClick={() => console.log('Enter clicked')}>
+        <button className="custom-button" onClick={handleClick}>
           Enter
         </button>
       </div>
